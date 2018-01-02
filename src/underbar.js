cddb6518618c0f6475dec3ee0cd5bf51b44cb53c
Michael <mqngo21@gmail.com>
2018-01-01 19:12:32 -0800

    Complete UnderBar 1

--- src/underbar.js
+++ src/underbar.js
@@ -125,6 +125,14 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var mapped = [];
+
+    for (var i = 0; i < collection.length; i++) {
+      var ele = collection[i];
+      mapped.push(iterator(ele));
+    }
+
+    return mapped;
   };
 
   /*
@@ -166,6 +174,17 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+    var initializing = accumulator === undefined;
+    _.each(collection, function(item) {
+      if (initializing) {
+        accumulator = item;
+        initializing = false;
+      } else {
+        accumulator = iterator(accumulator, item);
+      }
+    });
+    
+    return accumulator;
   };
 
   // Determine if the array or object contains a given value (using `===`).
@@ -184,6 +203,9 @@
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    return _.reduce(collection, function(match, item) {
+      return (iterator(item) === match);
+    })
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
